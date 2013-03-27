17f5f8ca08dda2d89efe4ae9eba49d84c7f0cef2
Enrique Ibarra <kikonio.dinamo@gmail.com>
2013-03-26 23:41:02 -0300

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Enrique Ibarra on 3/26/13.
+//  Copyright (c) 2013 Enrique Ibarra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Enrique Ibarra on 3/26/13.
+//  Copyright (c) 2013 Enrique Ibarra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
