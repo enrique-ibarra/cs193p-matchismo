99434878062ad62f6bb6e76724d2bab9d00a0142
Enrique Ibarra <kikonio.dinamo@gmail.com>
2013-03-27 07:41:02 -0300

    + initial import

---
+++ Matchismo/Model/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Matchismo
+//
+//  Created by Enrique Ibarra on 3/27/13.
+//  Copyright (c) 2013 Enrique Ibarra. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
---
+++ Matchismo/Model/Deck.h
@@ -0,0 +1,13 @@
+//
+//  Deck.h
+//  Matchismo
+//
+//  Created by Enrique Ibarra on 3/27/13.
+//  Copyright (c) 2013 Enrique Ibarra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Deck : NSObject
+
+@end
